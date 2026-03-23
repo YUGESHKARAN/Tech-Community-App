1281d5f9b870f213ad204be5670b8bf15e512d0b
YUGESHKARAN <yugeshkaran01@gmail.com>
2026-03-23 15:57:19 +0530

    playlist view page updated

--- frontend/src/Pages/YourPlaylists.jsx
+++ frontend/src/Pages/YourPlaylists.jsx
@@ -167,7 +167,7 @@ useEffect(() => {
               md:grid-cols-2
               lg:grid-cols-3
               xl:grid-cols-4
-              2xl:grid-cols-5
+              3xl:grid-cols-5
               overflow-visible
               space-y-5
               md:space-y-0
--- frontend/src/components/loaders/TutorPlaylistGridSkeleton.jsx
+++ frontend/src/components/loaders/TutorPlaylistGridSkeleton.jsx
@@ -56,7 +56,7 @@ const TutorPlaylistGridSkeleton = () => {
     md:grid-cols-2
     lg:grid-cols-3
     xl:grid-cols-4
-    2xl:grid-cols-5
+    3xl:grid-cols-5
     overflow-visible
     space-y-5
     md:space-y-0
