e57f527f23c422b949cc276113c331d57e13c0c5
YUGESHKARAN <yugeshkaran01@gmail.com>
2026-04-07 21:59:30 +0530

    Fixed: ReDOS attack in search engine. resolves #79

--- frontend/src/hooks/highlightText.jsx
+++ frontend/src/hooks/highlightText.jsx
@@ -1,22 +1,50 @@
+// const escapeRegExp = (string) => {
+//   return string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+// };
 
-const highlightText = (text, query) => {
-  if (!query) return text;
+// const highlightText = (text, query) => {
+//   if (!query || !text) return text;
 
-  const regex = new RegExp(`(${query})`, "gi");
-  const parts = text.split(regex);
+//   const escapedQuery = escapeRegExp(query);
 
- return parts.map((part, i) =>
-  part.toLowerCase() === query.toLowerCase() ? (
-    <span
-      key={i}
-      className="text-teal-500 font-semibold"
-    >
-      {part}
-    </span>
-  ) : (
-    part
-  )
-);
+//   const regex = new RegExp(`(${escapedQuery})`, "gi");
+//   const parts = text.split(regex);
+
+//   return parts.map((part, i) =>
+//     part.toLowerCase() === query.toLowerCase() ? (
+//       <span key={i} className="text-teal-500 font-semibold">
+//         {part}
+//       </span>
+//     ) : (
+//       part
+//     )
+//   );
+// };
+
+// export default highlightText;
+
+
+const escapeRegExp = (string) => {
+  return string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
 };
 
-export default highlightText
+const highlightText = (text, query) => {
+  if (!query || !text) return text;
+
+  const escapedQuery = escapeRegExp(query);
+
+  const regex = new RegExp(`(${escapedQuery})`, "gi");
+  const parts = text.split(regex);
+
+  return parts.map((part, i) =>
+    part.toLowerCase() === query.toLowerCase() ? (
+      <span key={i} className="text-teal-500 font-semibold">
+        {part}
+      </span>
+    ) : (
+      part
+    )
+  );
+};
+
+export default highlightText;
