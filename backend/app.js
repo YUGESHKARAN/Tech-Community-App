4110cd453d4756d26f351043b298e0e67c158b88
YUGESHKARAN <yugeshkaran01@gmail.com>
2026-05-04 16:17:15 +0530

    backend vercel.json updated

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,73 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+// const http = require("http");
+const serverless = require("serverless-http");
+const connectToDatabase = require("./db");
+const bodyParser = require("body-parser");
+const {limiter, loginLimiter} = require("./middleware/rateLimitter")
+require("dotenv").config();
+
+const app = express();
+
+// Connect to MongoDB
+connectToDatabase();
+
+// Middleware setup
+app.use(
+  cors({
+    origin: ["https://blog-frontend-teal-ten.vercel.app","http://localhost:5173"],// Match your frontend domain
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+
+// app.options("*", cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+// app.use(limiter);
+
+
+// Routes
+const loginRouter = require("./routes/login.Route");
+const authorRouter = require("./routes/authorDetail.Route");
+const postRouter = require("./routes/postDetail.Route");
+const tutorPlaylist = require("./routes/tutorPlayList.Route");
+
+// Admin route
+const appAnalysisRouter = require("./routes/appAnalysis.Route");
+const adminRouter = require("./routes/admin.Route")
+
+app.use("/blog/login", loginRouter);
+app.use("/blog/author", authorRouter);
+app.use("/blog/posts", postRouter);
+app.use("/blog/playlist",tutorPlaylist);
+
+app.use("/blog/analytics", appAnalysisRouter);
+app.use("/blog/admin", adminRouter);
+
+// Trust proxy (for rate limiter IP detection)
+app.set("trust proxy", 1);
+
+// Request timeout middleware
+app.use((req, res, next) => {
+  req.setTimeout(5000, () => res.status(504).send("Request timed out."));
+  next();
+});
+
+
+// Start the server
+// app.listen(3000, () => {
+//   console.log(`Server running on port ${3000}`);
+// });
+
+
+module.exports = app;
+module.exports.handler = serverless(app);
 // const express = require("express");
 // const cors = require("cors");
 // const path = require("path");
