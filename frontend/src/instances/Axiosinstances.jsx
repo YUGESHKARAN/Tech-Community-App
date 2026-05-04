9c8e3130bf7febf66254d9d3d684b071f680512b
YUGESHKARAN <yugeshkaran01@gmail.com>
2026-05-04 14:23:07 +0530

    CORS policy updated

--- frontend/src/instances/Axiosinstances.jsx
+++ frontend/src/instances/Axiosinstances.jsx
@@ -4,7 +4,7 @@ import Cookies from 'js-cookie';
 
 const axiosInstance = axios.create({
   // baseURL: 'http://localhost:3000/',
-  baseURL: 'https://node-blog-app-seven.vercel.app',
+  baseURL: 'https://node-blog-app-seven.vercel.app/',
   // withCredentials: true, // needed for cookies
 });
 
