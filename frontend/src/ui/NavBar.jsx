73e8e472ed9a6444e79ee9a713341220316c1a98
YUGESHKARAN <yugeshkaran01@gmail.com>
2026-05-16 20:13:10 +0530

    Notification icon updated - 2

--- frontend/src/ui/NavBar.jsx
+++ frontend/src/ui/NavBar.jsx
@@ -338,7 +338,7 @@ function NavBar() {
           {/* <IoMdNotifications */}
           <RiNotification3Line
             onClick={() => setShowNotification(!showNotefication)}
-            className="text-xl text-gray-300 hover:text-white transition-all duration-300 cursor-pointer transition"
+            className="text-lg text-gray-300 hover:text-white transition-all duration-300 cursor-pointer transition"
           />
           {notiCount > 0 && (
             <span className="absolute -top-1.5 -right-2 text-[10px] bg-red-500 w-4 h-4 flex items-center justify-center rounded-full text-white">
