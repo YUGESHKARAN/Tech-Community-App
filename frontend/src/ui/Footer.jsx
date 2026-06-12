d1f41fcdfaf636d9d4d96fa3b9d6536e0e9ced7c
YUGESHKARAN <yugeshkaran01@gmail.com>
2026-06-12 08:40:16 +0530

    Footer updated.

--- frontend/src/ui/Footer.jsx
+++ frontend/src/ui/Footer.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 function Footer() {
   return (
     // <div className='flex justify-center  items-center text-sm h-7 bg-[#091533] mt-5  text-white px-5'>
-    <div className='flex justify-center  items-center text-xs md:text-sm h-7 bg-gray-900 mt-5  text-white px-5'>
-    &#169; All Rights Reserved by NEOTERIC, 2026
+    <div className='flex justify-center  items-center text-xs h-7 bg-gray-900 mt-5  text-gray-200 px-5'>
+    &#169; All Rights Reserved by Bytes Base, 2026
     </div>
   )
 }
