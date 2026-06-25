ddeaea4202eb48e692cfd76ab102d865fabe1a1a
YUGESHKARAN <yugeshkaran01@gmail.com>
2026-06-25 12:49:19 +0530

    Add post guidelines updated.

--- frontend/src/Pages/AddPost.jsx
+++ frontend/src/Pages/AddPost.jsx
@@ -593,7 +593,7 @@ function AddPost() {
 
                 <ul className={`space-y-2 text-sm list-disc pl-5 hidden md:block`}>
                   <li>Use a clear and descriptive title.</li>
-                  <li>Provide a contextual description <span className="bg-gray-600/30 text-xs px-2">(eg: Overview, Tech-Stack used, Sample Code Snippet, System Design Followed, Key Features, Contributions and Conclusion)</span> </li>
+                  <li>Provide a contextual description <span className="bg-gray-600/30 text-xs font-semibold px-2">(eg: Overview, Tech-Stack used, Sample Code Snippet, System Design Followed, Key Features, Contributions and Conclusion)</span> </li>
                   <li>Description supports upto 10,000 word limit.</li>
                   <li>Add useful resources such as links and documents.</li>
                   <li>Include a suitable thumbnail poster (1280 × 720 px).</li>
@@ -635,7 +635,7 @@ function AddPost() {
                                 >
                                    <ul className={`space-y-2 text-sm list-disc pl-5 `}>
                                       <li>Use a clear and descriptive title.</li>
-                                      <li>Provide a contextual description <span className="bg-gray-600/30 text-xs px-2">(eg: Overview, Tech-Stack used, Sample Code Snippet, System Design Followed, Key Features, Contributions and Conclusion)</span> </li>
+                                      <li>Provide a contextual description <span className="bg-gray-600/30 text-xs font-semibold px-2">(eg: Overview, Tech-Stack used, Sample Code Snippet, System Design Followed, Key Features, Contributions and Conclusion)</span> </li>
                                       <li>Description supports upto 10,000 word limit.</li>
                                       <li>Add useful resources such as links and documents.</li>
                                       <li>Include a suitable thumbnail poster (1280 × 720 px).</li>
