859371b28035a0d7d586fe49af4912e746713e68
YUGESHKARAN <yugeshkaran01@gmail.com>
2026-06-30 19:37:45 +0530

    Profile page UI update - 2.

--- frontend/src/components/loaders/ProfilePageSkeleton.jsx
+++ frontend/src/components/loaders/ProfilePageSkeleton.jsx
@@ -5,30 +5,34 @@ const ProfilePageSkeleton = () => {
       {/* Header */}
       <div className="flex items-center justify-between mb-6">
         <div>
-          <div className="h-3 w-20 rounded bg-gray-800 animate-pulse mb-3" />
-          <div className="h-8 w-44 rounded bg-gray-800 animate-pulse" />
+          <div className="h-3 w-16 md:w-20 rounded bg-gray-800 animate-pulse mb-3" />
+          <div className="md:h-8 h-5 w-28 md:w-44 rounded bg-gray-800 animate-pulse" />
         </div>
 
         <div className="flex gap-3">
-          <div className="h-10 w-32 rounded-xl bg-gray-800 animate-pulse" />
-          <div className="h-10 w-36 rounded-xl bg-gray-800 animate-pulse" />
+          <div className="md:h-10 h-7 w-24 md:w-36 rounded-lg md:rounded-xl bg-gray-800 animate-pulse" />
+          <div className="h-10 w-36 hidden md:block rounded-xl bg-gray-800 animate-pulse" />
         </div>
       </div>
 
-      <div className="grid md:grid-cols-[300px_1fr]  gap-4">
+      <div className="md:grid flex flex-col md:grid-cols-[300px_1fr] w-full  gap-4">
         {/* LEFT */}
         <div className="rounded-2xl border h-fit border-white/10 bg-[#101827] p-6">
-          <div className="mx-auto h-24 w-24 rounded-full bg-gray-800 animate-pulse" />
-
-          <div className="mt-5 h-5 w-36 mx-auto rounded bg-gray-800 animate-pulse" />
-          <div className="mt-2 h-3 w-48 mx-auto rounded bg-gray-800 animate-pulse" />
+        <div className="flex md:block  justify-start items-start gap-2">
+           <div className="md:mx-auto h-16 shrink-0 w-16 md:h-24 md:w-24 rounded-full bg-gray-800 animate-pulse" />
+        <div className="flex flex-col items-start justify-start">
+          <div className="mt-5 h-5 w-36 md:mx-auto rounded bg-gray-800 animate-pulse" />
+          <div className="mt-2 h-3 w-48 md:mx-auto rounded bg-gray-800 animate-pulse" />
+        </div>
+        </div>
+         
 
           <div className="mt-8">
             <div className="h-3 w-14 rounded bg-gray-800 animate-pulse mb-4" />
 
             <div className="space-y-2">
               <div className="h-3 rounded bg-gray-800 animate-pulse" />
-              <div className="h-3 w-11/12 rounded bg-gray-800 animate-pulse" />
+              <div className="h-3 md:w-11/12 rounded bg-gray-800 animate-pulse" />
               <div className="h-3 w-3/4 rounded bg-gray-800 animate-pulse" />
             </div>
           </div>
