7059142b67ec8402e36f101ad4199ec650503e63
YUGESHKARAN <yugeshkaran01@gmail.com>
2026-07-04 18:46:14 +0530

    fix: view profile card backdrop fixed.

--- frontend/src/Pages/ViewSingleAuthor.jsx
+++ frontend/src/Pages/ViewSingleAuthor.jsx
@@ -492,7 +492,7 @@ function ViewSingleAuthor() {
                       exit={{ opacity: 0 }}
                       onClick={() => setShowFollows(false)}
                       className="
-                        fixed inset-0 z-50
+                        absolute inset-0 z-50
                         flex items-center justify-center
                         bg-black/70  h-full     
                       "
